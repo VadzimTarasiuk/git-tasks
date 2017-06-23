8342301d880537e98a965ce412c83aec12e78c07
Forbear <silnyagindan@gmail.com>
2017-06-23 14:08:57 +0300

    init nginx script

--- script_daniil.sh
+++ script_daniil.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 yum install nginx -y
+
+echo [Unit] > tmp
+echo Description=Nginx start >> tmp
+echo After=network.target remote-fs.target nss-lookup.target >> tmp
+echo [Service] >> tmp
+echo Type=forking >> tmp
+#echo Environment=JAVA_HOME=/opt/oracle/java/x64/jdk1.7.0_79/jre >> tmp
+echo ExecStart=nginx >> tmp
+echo PrivateTmp=true >> tmp
+echo [Install] >> tmp
+echo WantedBy=multi-user.target >> tmp
+mv tmp /usr/lib/systemd/system/nginxinit.service
+systemctl enable nginxinit.service
+
 #tar -xf java.tar.gz
 #JAVA_HOME=./jdk1.8.0_131/jre
 #export JAVA_HOME
